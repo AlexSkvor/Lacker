8b4e1142541441f3e73d765b796657cb2fc20bad
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2020-09-08 10:35:03 +0200

    Add new utils

---
+++ utils/src/main/java/com/alexskvor/utils/extensions/AnimationExtensions.kt
@@ -0,0 +1,42 @@
+package com.alexskvor.utils.extensions
+
+import android.view.View
+import android.view.animation.*
+import androidx.annotation.AnimRes
+
+fun View.animateFade() {
+
+    val fadeOut = AlphaAnimation(1f, 0.5f)
+    fadeOut.interpolator = AccelerateInterpolator()
+    fadeOut.duration = 50
+
+    val fadeIn = AlphaAnimation(0.5f, 1f)
+    fadeIn.interpolator = DecelerateInterpolator()
+    fadeIn.startOffset = 50
+    fadeIn.duration = 50
+
+    val animation = AnimationSet(false)
+    animation.addAnimation(fadeIn)
+    animation.addAnimation(fadeOut)
+    startAnimation(animation)
+}
+
+fun View.animate(@AnimRes animId: Int) {
+    startAnimation(AnimationUtils.loadAnimation(context, animId))
+}
+
+fun View.animateScale(onAnimationMiddle: (() -> Unit)? = null) {
+    animate().apply {
+        scaleX(0.97f)
+        scaleY(0.97f)
+        duration = 50
+        withEndAction {
+            onAnimationMiddle?.invoke()
+            animate().apply {
+                scaleX(1f)
+                scaleY(1f)
+                duration = 50
+            }
+        }
+    }
+}
