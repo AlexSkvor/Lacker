5f74ef1480a7f1efd626a2e6d717a50f72103924
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-01-04 16:30:09 +0200

    Add screens stubs

--- app/src/main/java/com/lacker/visitors/navigation/Screens.kt
+++ app/src/main/java/com/lacker/visitors/navigation/Screens.kt
@@ -6,6 +6,22 @@ import com.lacker.visitors.features.auth.AuthFragment
 
 object Screens {
 
+    data class AuthFlow(val hasScreensBefore: Boolean) : SupportAppScreen() {
+        override fun getFragment(): Fragment = TODO()
+    }
+
+    data class LackerSignIn(val hasScreensBefore: Boolean) : SupportAppScreen() {
+        override fun getFragment(): Fragment = TODO()
+    }
+
+    data class LackerSignUp(val hasScreensBefore: Boolean) : SupportAppScreen() {
+        override fun getFragment(): Fragment = TODO()
+    }
+
+    object StuffSignIn : SupportAppScreen() {
+        override fun getFragment(): Fragment = TODO()
+    }
+
     object AuthScreen : SupportAppScreen() {
         override fun getFragment(): Fragment = AuthFragment.newInstance()
     }
