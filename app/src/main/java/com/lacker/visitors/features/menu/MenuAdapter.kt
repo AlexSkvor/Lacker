2db53039c4f765e6590116c78e86b064ea676bf8
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-01-22 11:30:13 +0200

    Create MenuAdapter simplest implementation

---
+++ app/src/main/java/com/lacker/visitors/features/menu/MenuAdapter.kt
@@ -0,0 +1,38 @@
+package com.lacker.visitors.features.menu
+
+import androidx.recyclerview.widget.DiffUtil
+import com.hannesdorfmann.adapterdelegates4.AsyncListDifferDelegationAdapter
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateLayoutContainer
+import com.lacker.utils.extensions.loadFromNet
+import com.lacker.visitors.R
+import kotlinx.android.synthetic.main.item_menu_item.*
+
+fun getMenuAdapter(
+    onAddToOrder: (DomainPortion) -> Unit,
+    onItemClick: (DomainMenuItem) -> Unit
+) = AsyncListDifferDelegationAdapter(
+    object : DiffUtil.ItemCallback<DomainMenuItem>() {
+        override fun areItemsTheSame(oldItem: DomainMenuItem, newItem: DomainMenuItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: DomainMenuItem, newItem: DomainMenuItem): Boolean {
+            return oldItem == newItem
+        }
+    },
+    getDomainMenuItemAdapter(onAddToOrder, onItemClick)
+)
+
+fun getDomainMenuItemAdapter(
+    onAddToOrder: (DomainPortion) -> Unit,
+    onItemClick: (DomainMenuItem) -> Unit
+) = adapterDelegateLayoutContainer<DomainMenuItem, DomainMenuItem>(R.layout.item_menu_item) {
+
+    bind {
+        menuItemName.text = item.name
+        menuItemDescription.text = item.shortDescription
+
+        menuItemPicture.loadFromNet(url = item.photoFullUrl)
+    }
+
+}
