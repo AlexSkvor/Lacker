932e5b1cae530dd85308a5eebf71999f6b29aa7a
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-02-22 14:53:49 +0200

    Prepare for receiving tokenId from google

--- app/src/main/java/com/lacker/visitors/di/AppModule.kt
+++ app/src/main/java/com/lacker/visitors/di/AppModule.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.lacker.visitors.R
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -19,6 +20,7 @@ class AppModule(private val context: Context) {
     @Provides
     fun provideGoogleApiClient(): GoogleSignInClient {
         val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(context.getString(R.string.client_id))
             .requestEmail()
             .build()
 
