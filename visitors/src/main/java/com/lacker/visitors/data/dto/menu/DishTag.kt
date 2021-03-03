eb6272dfeb9b16ced99269d945578555eee23ede
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-03-03 13:03:20 +0200

    Rename app module to visitors

---
+++ visitors/src/main/java/com/lacker/visitors/data/dto/menu/DishTag.kt
@@ -0,0 +1,32 @@
+package com.lacker.visitors.data.dto.menu
+
+enum class DishTag {
+
+    Drink, // Напиток
+    Pizza, // Пицца
+    Soup, // Суп
+    Snack, // Закуски
+    ColdSnack, // Холодные закуски
+    HotSnack, // Горячие закуски
+    Starter, // Первые блюда
+    Salad, // Салат
+    MainDish, // Основное блюдо
+    Meat, // С мясом
+    Steak, // Стейк
+    Poultry, // Блюдо из птицы
+    Fish, // Блюдо из рыбы
+    Seafood, // Морепродукты
+    SideDish, // Гарнир
+    Sauce, // Соус
+    Dessert, // Десерт
+    SoftDrink, // Прохладительный напиток
+    HotDrink, // Горячий напиток
+    DrinkAlcohol, // Алкогольный напиток
+    Breakfast, // Завтрак
+    Lunch, // Ланч
+    WOK, // Лапша вок
+    BBQ, // Барбикю
+    Burger, // Бургер
+    Sushi, // Суши
+
+}
