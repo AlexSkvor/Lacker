6d82b0265ea393f9a7b7b6534f30584fa17e34ee
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-04-03 17:18:57 +0200

    Start implementing pagination asks

--- staff/src/main/java/com/lacker/staff/features/orders/TasksMachine.kt
+++ staff/src/main/java/com/lacker/staff/features/orders/TasksMachine.kt
@@ -11,6 +11,7 @@ import com.lacker.utils.extensions.onNull
 import com.lacker.utils.resources.ResourceProvider
 import ru.terrakok.cicerone.Router
 import voodoo.rocks.flux.Machine
+import voodoo.rocks.paginator.reduce.Ask
 import voodoo.rocks.paginator.reduce.PaginationList
 
 class TasksMachine @Inject constructor(
@@ -19,7 +20,8 @@ class TasksMachine @Inject constructor(
 ) : Machine<Wish, Result, State>() {
 
     sealed class Wish {
-
+        data class ChangeShowType(val type: State.Type) : Wish()
+        data class PaginationAsk(val type: State.Type, val ask: Ask) : Wish()
     }
 
     sealed class Result {
@@ -31,6 +33,8 @@ class TasksMachine @Inject constructor(
         val oldOrders: PaginationList<SubOrderListItem> = PaginationList.EmptyProgress(),
         val newCalls: PaginationList<StaffCall> = PaginationList.EmptyProgress(),
         val oldCalls: PaginationList<StaffCall> = PaginationList.EmptyProgress(),
+        val newOrdersTotalCount: Int = 0,
+        val newCallsTotalCount: Int = 0,
         val type: Type = Type.NEW_ORDERS
     ) {
         enum class Type {
@@ -40,7 +44,25 @@ class TasksMachine @Inject constructor(
 
     override val initialState: State = State()
 
-    override fun onWish(wish: Wish, oldState: State): State = oldState
+    override fun onWish(wish: Wish, oldState: State): State = when (wish) {
+        is Wish.ChangeShowType -> oldState.copy(type = wish.type)
+        is Wish.PaginationAsk -> onPaginationAsk(oldState, wish.type, wish.ask)
+    }
+
+    private fun onPaginationAsk(oldState: State, type: State.Type, ask: Ask): State = when (type) {
+        State.Type.NEW_ORDERS -> oldState.copy(newOrders = oldState.newOrders.onAsk(ask) {
+            // TODO push result
+        })
+        State.Type.NEW_CALLS -> oldState.copy(newCalls = oldState.newCalls.onAsk(ask) {
+            // TODO push result
+        })
+        State.Type.OLD_ORDERS -> oldState.copy(oldOrders = oldState.oldOrders.onAsk(ask) {
+            // TODO push result
+        })
+        State.Type.OLD_CALLS -> oldState.copy(oldCalls = oldState.oldCalls.onAsk(ask) {
+            // TODO push result
+        })
+    }
 
     override fun onResult(res: Result, oldState: State): State = oldState
 
