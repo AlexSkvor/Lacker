c3db32f32831585073c3fb482633ae2eb2004e9f
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-04-03 16:09:38 +0200

    Add field names for StaffCall

--- staff/src/main/java/com/lacker/staff/data/dto/calls/StaffCall.kt
+++ staff/src/main/java/com/lacker/staff/data/dto/calls/StaffCall.kt
@@ -1,12 +1,14 @@
 package com.lacker.staff.data.dto.calls
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class StaffCall(
-    val tableName: String,
-    val clientName: String,
-    val callType: CallType
+    @Json(name = "id") val id: String,
+    @Json(name = "tableName") val tableName: String,
+    @Json(name = "clientName") val clientName: String,
+    @Json(name = "callType") val callType: CallType
 )
 
 enum class CallType {
