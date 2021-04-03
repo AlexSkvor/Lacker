5c53ddaec90aed3560f31db0ed811417610bf9d9
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-04-03 17:19:23 +0200

    Implement TasksFragment partially

--- staff/src/main/java/com/lacker/staff/features/orders/TasksFragment.kt
+++ staff/src/main/java/com/lacker/staff/features/orders/TasksFragment.kt
@@ -1,12 +1,40 @@
 package com.lacker.staff.features.orders
 
+import androidx.recyclerview.widget.RecyclerView
 import com.lacker.staff.R
 import com.lacker.utils.base.ToolbarFluxFragment
 import com.lacker.utils.base.ToolbarFragmentSettings
 import com.lacker.staff.features.orders.TasksMachine.Wish
 import com.lacker.staff.features.orders.TasksMachine.State
+import com.lacker.staff.features.orders.adapters.orderAdapter
+import com.lacker.staff.views.asDomain
+import com.lacker.staff.views.asUi
+import com.lacker.utils.extensions.*
+import kotlinx.android.synthetic.main.fragment_tasks.*
+import voodoo.rocks.paginator.reduce.Ask
 
 class TasksFragment : ToolbarFluxFragment<Wish, State>() {
+
+    /**
+     * Вкладки:
+     * 1) Новые подзаказы
+     * 2) Новые вызовы
+     * 3) Учтенные подзаказы
+     * 4) Учтенные вызовы
+     *
+     * Подзаказ:
+     * - имя столика
+     * - имя клиента
+     * - число блюд
+     * * посмотреть
+     * * обработать
+     *
+     * Для пагинации используем "Опорный элемент", то есть передается id подзаказа / вызова и
+     * указывается сколько элементов, которые старше опорного по времени создания вернуть
+     *
+     * Из деталей подзаказа можно посмотреть весь заказ
+     */
+
     companion object {
         fun newInstance() = TasksFragment()
     }
@@ -15,20 +43,135 @@ class TasksFragment : ToolbarFluxFragment<Wish, State>() {
 
     override val machine by lazy { getMachineFromFactory(TasksMachine::class.java) }
 
-    override val toolbarSettings: ToolbarFragmentSettings by lazy {
-        ToolbarFragmentSettings(
-            title = getString(R.string.tasksScreenTitle),
+    override val toolbarSettings: ToolbarFragmentSettings
+        get() = ToolbarFragmentSettings(
+            title = currentTitle,
             subtitle = null,
             showBackIcon = false,
             menuResId = null
         )
-    }
+
+    private val currentTitle: String
+        get() = when (machine.states().value.type) {
+            State.Type.NEW_ORDERS -> getString(R.string.newOrdersScreenTitle)
+            State.Type.NEW_CALLS -> getString(R.string.newCallsScreenTitle)
+            State.Type.OLD_ORDERS -> getString(R.string.oldOrdersScreenTitle)
+            State.Type.OLD_CALLS -> getString(R.string.oldCallsScreenTitle)
+        }
+
+    private val allViews
+        get() = listOf(
+            newOrdersPaginationview,
+            newCallsPaginationview,
+            oldOrdersPaginationview,
+            oldCallsPaginationview
+        )
+
+    private val newOrdersAdapter by lazy { orderAdapter() }
 
     override fun onScreenInit() {
-        //TODO("Not yet implemented")
+        setupPaginationViews()
+        setupNavigationView()
+        tasksNavigationBar.onStateChange {
+            performWish(Wish.ChangeShowType(it.asDomain()))
+        }
+    }
+
+    private fun setupPaginationViews() {
+        newOrdersPaginationview.apply {
+            onAsk { performWish(Wish.PaginationAsk(State.Type.NEW_ORDERS, it)) }
+            addOrReplaceExistingAdapter(newOrdersAdapter)
+        }
+        newCallsPaginationview.apply {
+            onAsk { performWish(Wish.PaginationAsk(State.Type.NEW_CALLS, it)) }
+            addOrReplaceExistingAdapter(newOrdersAdapter) // TODO different adapter
+        }
+        oldOrdersPaginationview.apply {
+            onAsk { performWish(Wish.PaginationAsk(State.Type.OLD_ORDERS, it)) }
+            addOrReplaceExistingAdapter(newOrdersAdapter) // TODO different adapter
+        }
+        oldCallsPaginationview.apply {
+            onAsk { performWish(Wish.PaginationAsk(State.Type.OLD_CALLS, it)) }
+            addOrReplaceExistingAdapter(newOrdersAdapter) // TODO different adapter
+        }
+
+        State.Type.values().forEach {
+            performWish(Wish.PaginationAsk(it, Ask.Refresh))
+        }
+    }
+
+    private fun setupNavigationView() {
+        allViews.forEach {
+            it.findViewById<RecyclerView>(R.id.recyclerView).onScroll { upper ->
+                if (upper) tasksNavigationBar.appearFromBottom(200)
+                else tasksNavigationBar.hideBelowBottom(200)
+            }
+        }
     }
 
     override fun render(state: State) {
-        //TODO("Not yet implemented")
+        refreshToolbar()
+        renderTasksNavigationBar(
+            type = state.type,
+            ordersNumber = state.newOrdersTotalCount,
+            callsNumber = state.newCallsTotalCount
+        )
+        renderVisibleView(state.type)
+        renderList(state)
     }
+
+    private fun renderTasksNavigationBar(type: State.Type, ordersNumber: Int, callsNumber: Int) {
+        tasksNavigationBar.setState(type.asUi())
+        tasksNavigationBar.setNewOrdersBadge(ordersNumber)
+        tasksNavigationBar.setNewCallsBadge(callsNumber)
+    }
+
+    private fun renderList(state: State) {
+        when (state.type) {
+            State.Type.NEW_ORDERS -> newOrdersPaginationview.setList(state.newOrders)
+            State.Type.NEW_CALLS -> newCallsPaginationview.setList(state.newCalls)
+            State.Type.OLD_ORDERS -> oldOrdersPaginationview.setList(state.oldOrders)
+            State.Type.OLD_CALLS -> oldCallsPaginationview.setList(state.oldCalls)
+        }
+    }
+
+    private var prevStateType: State.Type? = null
+    private fun renderVisibleView(nextStateType: State.Type) {
+
+        fun shouldAppearFromRight(newType: State.Type): Boolean {
+            val oldPos = State.Type.values().indexOf(prevStateType)
+            val newPos = State.Type.values().indexOf(newType)
+            return oldPos < newPos
+        }
+
+        val nextVisibleView = when (nextStateType) {
+            State.Type.NEW_ORDERS -> newOrdersPaginationview
+            State.Type.NEW_CALLS -> newCallsPaginationview
+            State.Type.OLD_ORDERS -> oldOrdersPaginationview
+            State.Type.OLD_CALLS -> oldCallsPaginationview
+        }
+        val previousVisibleView = allViews.firstOrNull { it.visible }
+
+        allViews.forEach {
+            if (it != previousVisibleView && it != nextVisibleView)
+                it.gone()
+        }
+        val typeNotChanged = prevStateType == nextStateType || prevStateType == null
+        if (typeNotChanged) {
+            if (previousVisibleView != nextVisibleView)
+                previousVisibleView?.gone()
+            nextVisibleView.visible()
+            nextVisibleView.x = 0f
+        } else {
+            if (shouldAppearFromRight(nextStateType)) {
+                previousVisibleView?.hideOnLeft(300L)
+                nextVisibleView.appearFromRight(300L)
+            } else {
+                previousVisibleView?.hideOnRight(300L)
+                nextVisibleView.appearFromLeft(300L)
+            }
+        }
+        prevStateType = nextStateType
+    }
+
 }
---
+++ staff/src/main/java/com/lacker/staff/features/orders/adapters/OrderAdapter.kt
@@ -0,0 +1,15 @@
+package com.lacker.staff.features.orders.adapters
+
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateLayoutContainer
+import com.lacker.staff.R
+import com.lacker.staff.data.dto.orders.SubOrderListItem
+
+fun orderAdapter(
+
+) = adapterDelegateLayoutContainer<SubOrderListItem, Any>(R.layout.item_header) {
+
+    bind {
+
+    }
+
+}
