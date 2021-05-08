bc57419a4d00a3e3c9a8486a1650766d821cb1f9
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-05-08 13:27:39 +0200

    Create RestaurantStorage

---
+++ staff/src/main/java/com/lacker/staff/data/storage/restaurants/DomainRestaurant.kt
@@ -0,0 +1,5 @@
+package com.lacker.staff.data.storage.restaurants
+
+data class DomainRestaurant(
+    val id: String,
+)
--- staff/src/main/java/com/lacker/staff/di/DataModule.kt
+++ staff/src/main/java/com/lacker/staff/di/DataModule.kt
@@ -1,5 +1,7 @@
 package com.lacker.staff.di
 
+import com.lacker.staff.data.storage.restaurants.RestaurantPrefs
+import com.lacker.staff.data.storage.restaurants.RestaurantStorage
 import com.lacker.staff.data.storage.restaurants.SignedBeforeRestaurantsPrefs
 import com.lacker.staff.data.storage.restaurants.SignedBeforeRestaurantsStorage
 import com.lacker.staff.data.storage.user.UserPrefs
@@ -14,6 +16,10 @@ interface DataModule {
     @Singleton
     @Binds
     fun bindUserStorage(userPrefs: UserPrefs): UserStorage
+
+    @Singleton
+    @Binds
+    fun bindRestaurantStorage(restaurantPrefs: RestaurantPrefs): RestaurantStorage
 
     @Singleton
     @Binds
