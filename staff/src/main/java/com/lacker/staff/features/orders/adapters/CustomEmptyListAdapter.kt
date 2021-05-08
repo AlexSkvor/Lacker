5097380222d599efe692cbb2026154ee068bc0a5
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-05-08 17:23:40 +0200

    Add todo for tomorrow

--- staff/src/main/java/com/lacker/staff/features/orders/adapters/CustomEmptyListAdapter.kt
+++ staff/src/main/java/com/lacker/staff/features/orders/adapters/CustomEmptyListAdapter.kt
@@ -5,6 +5,7 @@ import com.lacker.staff.R
 import kotlinx.android.synthetic.main.item_empty_list.*
 import voodoo.rocks.paginator.adapters.EmptyItem
 
+// TODO other custom adapters
 fun customEmptyListAdapter(
     onRefresh: () -> Unit,
 ) = adapterDelegateLayoutContainer<EmptyItem, Any>(R.layout.item_empty_list) {
