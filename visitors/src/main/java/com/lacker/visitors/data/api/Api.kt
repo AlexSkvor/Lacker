91df1053490a2feb6bff20c25ce3cd95c6aab989
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-05-17 15:53:23 +0200

    Add todo

--- visitors/src/main/java/com/lacker/visitors/data/api/Api.kt
+++ visitors/src/main/java/com/lacker/visitors/data/api/Api.kt
@@ -27,7 +27,7 @@ interface Api {
         @Path("restaurantId") restaurantId: String
     ): DateTimeResponse
 
-    @Headers(AuthHeaderInterceptor.REQUIRES_AUTH)
+    @Headers(AuthHeaderInterceptor.REQUIRES_AUTH) // TODO remove, unneeded
     @GET("api/{restaurantId}/tables")
     suspend fun getTablesOfRestaurant(
         @Path("restaurantId") restaurantId: String,
