dcbb02c8b52b430a12bb37f00a7861f3bd958015
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-05-24 18:23:06 +0200

    Change json field name

--- dto/src/main/java/com/lacker/dto/order/SubOrder.kt
+++ dto/src/main/java/com/lacker/dto/order/SubOrder.kt
@@ -7,7 +7,7 @@ import java.time.OffsetDateTime
 @JsonClass(generateAdapter = true)
 data class SubOrder(
     @Json(name = "comment") val comment: String,
-    @Json(name = "drinksImmediately") val drinksImmediately: Boolean = false,
+    @Json(name = "drinks_immediately") val drinksImmediately: Boolean = false,
     @Json(name = "orderList") val orderList: List<OrderInfo>,
     @Json(name = "createdTimeStamp") val createdTimeStamp: OffsetDateTime?
 )
--- visitors/src/main/java/com/lacker/visitors/data/dto/order/AddSuborderRequest.kt
+++ visitors/src/main/java/com/lacker/visitors/data/dto/order/AddSuborderRequest.kt
@@ -6,6 +6,6 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class AddSuborderRequest(
     @Json(name = "comment") val comment: String,
-    @Json(name = "drinksImmediately") val drinksImmediately: Boolean,
+    @Json(name = "drinks_immediately") val drinksImmediately: Boolean,
     @Json(name = "portions") val portions: List<String>,
 )
