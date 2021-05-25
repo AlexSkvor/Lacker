9f3dddabe7d3c5db474ee973bda8cd4d0a952c90
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-05-25 12:29:41 +0200

    Implement filter listeners

--- visitors/src/main/java/com/lacker/visitors/features/session/filter/MenuFilterBottomFragment.kt
+++ visitors/src/main/java/com/lacker/visitors/features/session/filter/MenuFilterBottomFragment.kt
@@ -7,8 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.lacker.dto.menu.DishTag
+import com.lacker.dto.menu.DishTag.*
 import com.lacker.dto.menu.MenuSearchFilter
 import com.lacker.visitors.R
+import kotlinx.android.synthetic.main.bottom_sheet_fragment_menu_filter.*
 
 class MenuFilterBottomFragment : BottomSheetDialogFragment() {
 
@@ -36,8 +39,49 @@ class MenuFilterBottomFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // TODO listeners
-        // TODO preset state
+
+        menuFilterSearchField.setText(filter.text)
+
+        chipAlcohol.isChecked = ALCOHOL in filter.tags
+        chipDrinks.isChecked = DRINKS in filter.tags
+        chipBird.isChecked = BIRD in filter.tags
+        chipSea.isChecked = SEA in filter.tags
+        chipMeat.isChecked = MEAT in filter.tags
+        chipGarnish.isChecked = GARNISH in filter.tags
+        chipAsian.isChecked = ASIAN_DISH in filter.tags
+        chipDessert.isChecked = DESSERT in filter.tags
+        chipSalad.isChecked = SALAD in filter.tags
+        chipSandwich.isChecked = SANDWICH in filter.tags
+        chipSoup.isChecked = SOUP in filter.tags
+        chipOther.isChecked = OTHER in filter.tags
+
+        searchButton.setOnClickListener {
+            listener(getFiltersState())
+            dismiss()
+        }
+        clearButton.setOnClickListener {
+            listener(MenuSearchFilter(emptySet(), ""))
+            dismiss()
+        }
+    }
+
+    private fun getFiltersState(): MenuSearchFilter {
+        val set = mutableSetOf<DishTag>()
+
+        if (chipAlcohol.isChecked) set += ALCOHOL
+        if (chipDrinks.isChecked) set += DRINKS
+        if (chipBird.isChecked) set += BIRD
+        if (chipSea.isChecked) set += SEA
+        if (chipMeat.isChecked) set += MEAT
+        if (chipGarnish.isChecked) set += GARNISH
+        if (chipAsian.isChecked) set += ASIAN_DISH
+        if (chipDessert.isChecked) set += DESSERT
+        if (chipSalad.isChecked) set += SALAD
+        if (chipSandwich.isChecked) set += SANDWICH
+        if (chipSoup.isChecked) set += SOUP
+        if (chipOther.isChecked) set += OTHER
+
+        return MenuSearchFilter(set, menuFilterSearchField.text?.toString().orEmpty())
     }
 
 }
