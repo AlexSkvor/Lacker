05aa564c97769de723b02d1f18b6fb84008d7add
a.p.skvortsov <a.p.skvortsov@voodoo.rocks>
2021-05-27 15:58:36 +0200

    Implement name and table for suborders

--- dto/src/main/java/com/lacker/dto/order/SubOrder.kt
+++ dto/src/main/java/com/lacker/dto/order/SubOrder.kt
@@ -1,5 +1,7 @@
 package com.lacker.dto.order
 
+import com.lacker.dto.common.NameSurnameOwner
+import com.lacker.dto.common.TitleOwner
 import com.lacker.dto.menu.Portion
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
@@ -14,15 +16,15 @@ data class SubOrder(
     @Json(name = "count") val totalPrice: Int,
     @Json(name = "create_time") val createdTimeStamp: OffsetDateTime?,
     @Json(name = "checked") val checked: Boolean,
+    @Json(name = "table") val table: TitleOwner,
+    @Json(name = "user") val user: NameSurnameOwner,
 ) {
     val orderList: List<OrderInfo>
         get() = portions.groupBy { it.id }
             .toList()
             .map { OrderInfo(portionId = it.first, ordered = it.second.size) }
 
-    val clientName: String
-        get() = "TODO"
+    val clientName: String = user.name
 
-    val tableName: String
-        get() = "TODO"
+    val tableName: String = table.title
 }
